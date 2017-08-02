cf540ae7aeecb8c4f60085aaac10d740381efbe4
Sadain Iqrar <sadain.abbasi@gmail.com>
2017-08-02 15:05:24 +0500

    updated

---
+++ js/services.js
@@ -0,0 +1,71 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+function strategyService() {
+    document.getElementById("strategy").src = "img/creativity.png"
+    
+    document.getElementById("design").src = "img/paint-brush1.png"
+    
+    document.getElementById("market").src = "img/megaphone1.png"
+    
+    document.getElementById("development").src = "img/laptop1.png"
+    
+    
+    service.innerText = "Strategy and Consulting";
+    
+    servicetext.innerText = " We provide strategy and consulting services to decide what best fits your product and brand. The purpose is to effectively launch the brands into the digital space by providing a strong building platform.";
+    
+    return false;
+}
+
+function designService() {
+    
+    document.getElementById("strategy").src = "img/creativity1.png"
+    
+    document.getElementById("design").src = "img/paint-brush.png"
+    
+    document.getElementById("market").src = "img/megaphone1.png"
+    
+    document.getElementById("development").src = "img/laptop1.png"
+    service.innerText = "Branding and Design";
+    
+    servicetext.innerText = " We aim at highlighting your brand with unique and successful ideas that makes it stand out from the rest of market brands. Our custom designs are focused to emphasize the individual essence and creativity.";
+    
+    return false;
+}
+
+function marketService() {
+    
+    document.getElementById("strategy").src = "img/creativity1.png"
+    
+    document.getElementById("design").src = "img/paint-brush1.png"
+    
+    document.getElementById("market").src = "img/megaphone.png"
+    
+    document.getElementById("development").src = "img/laptop1.png"
+    service.innerText = "Digital Marketing";
+    
+    servicetext.innerText = "Our advanced marketing system is based on the establishment of driving measurable outcomes. We focus on expanding the traffic conversions, producing better brand loyalty online.";
+    
+    return false;
+}
+
+function developService() {
+    
+    document.getElementById("strategy").src = "img/creativity1.png"
+    
+    document.getElementById("design").src = "img/paint-brush1.png"
+    
+    document.getElementById("market").src = "img/megaphone1.png"
+    
+    document.getElementById("development").src = "img/laptop.png"
+    service.innerText = "Web Development";
+    
+    servicetext.innerText = "From big business level lead era and web based business sites to custom content management frameworks, our web developer team cater every area to deliver an effective website that you desire.";
+    
+    return false;
+}
